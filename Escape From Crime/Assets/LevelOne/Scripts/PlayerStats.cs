4e19e7e04593fba504a1cb2d6e87caa06d4cc64e
Mahmoud Khairi Al Masidi <m.almasidi@icloud.com>
2024-12-15 22:28:49 +0200

    .

--- Escape From Crime/Assets/LevelOne/Scripts/PlayerStats.cs
+++ Escape From Crime/Assets/LevelOne/Scripts/PlayerStats.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int health=6;
+    public int lives=3;
+
+    private float flickerTime=0f;
+    public float flickerDuration=0.1f;
+
+    private SpriteRenderer spriterenderer;
+
+    public bool isImmune = false;
+    private float immunityTime=0f;
+    public float immunityDuration=1.5f;
+
+    public int coinsCollected=0;
+
     void Start()
     {
-        
+        spriterenderer=this.gameObject.GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
+    void SpriteFlicker()
+    {
+        if(this.flickerTime<this.flickerDuration){
+            this.flickerTime=this.flickerTime+Time.deltaTime;
+        }
+        else if (this.flickerTime>=this.flickerDuration){
+            spriterenderer.enabled=!(spriterenderer.enabled);
+            this.flickerTime=0;
+        }
+    }
+    
     void Update()
     {
-        
+        if(this.isImmune==true){
+            SpriteFlicker();
+            immunityTime= immunityTime + Time.deltaTime;
+            if(immunityTime>=immunityDuration){
+                this.isImmune=false;
+                this.spriterenderer.enabled=true;
+            }
+        }
     }
+
+    public void TakeDamage(int damage){
+        if (this.isImmune==false){
+            this.health=this.health-damage;
+            if(this.health<0)
+            this.health=0;
+            if (this.lives>0 && this.health==0){
+                FindObjectOfType<LevelManager>().RespawnPlayer();
+                this.health=6;
+                this.lives--;
+            }
+            else if (this.lives==0 && this.health==0){
+                Debug.Log("Game Over");
+                Destroy(this.gameObject);
+            }
+            Debug.Log("Player Health: "+this.health.ToString());
+            Debug.Log("Player Lives: "+this.lives.ToString());
+        }
+        PlayHitReaction();
+    }
+
+    void PlayHitReaction(){
+        this.isImmune=true;
+        this.immunityTime=0f;
+    }
+
 }
